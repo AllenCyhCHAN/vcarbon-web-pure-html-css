be5f294c9aa645ab98b5f25ebec138c84e9a2989
AllenCHANcyh <allencyh2001@gmail.com>
2022-12-23 16:57:37 +0800

    finished additional requirement from eunice and blog page

--- js/medigo-custom.js
+++ js/medigo-custom.js
@@ -130,66 +130,6 @@ jQuery(document).ready(function($){
 	    });
 
 
-	/************** Contact Form *********************/
-	    $(".contact-form #submit").click(function() { 
-	        //collect input field values
-	        var user_name       = $('input[name=name]').val(); 
-	        var user_email      = $('input[name=email]').val();
-	        var user_subject      = $('input[name=subject]').val();
-	        var user_message    = $('textarea[name=message]').val();
-	        
-	        //simple validation at client's end
-	        //we simply change border color to red if empty field using .css()
-	        var proceed = true;
-	        if(user_name==""){ 
-	            $('input[name=name]').css('border-color','red'); 
-	            proceed = false;
-	        }
-	        if(user_email==""){ 
-	            $('input[name=email]').css('border-color','red'); 
-	            proceed = false;
-	        }
-	        if(user_subject=="") {    
-	            $('input[name=subject]').css('border-color','red'); 
-	            proceed = false;
-	        }
-	        if(user_message=="") {  
-	            $('textarea[name=message]').css('border-color','red'); 
-	            proceed = false;
-	        }
 
-	        	
-
-	        //everything looks good! proceed...
-	        if(proceed) 
-	        {
-	            //data to be sent to server
-	            post_data = {'userName':user_name, 'userEmail':user_email, 'userSubject':user_subject, 'userMessage':user_message};
-	            
-	            //Ajax post data to server
-	            $.post('contact.php', post_data, function(data){  
-	                
-	                //load success massage in #result div element, with slide effect.       
-	                $("#result").hide().html('<div class="success">'+data+'</div>').slideDown();
-
-	                //reset values in all input fields
-                	$('.widget-inner input').val(''); 
-                	$('.widget-inner textarea').val(''); 
-	                
-	            }).fail(function(err) {  //load any error data
-	                $("#result").hide().html('<div class="error">'+err.statusText+'</div>').slideDown();
-	            });
-	        }
-	                
-	    });
-	    
-	    //reset previously set border colors and hide all message on .keyup()
-	    $(".contact-form input, .contact-form textarea").keyup(function() { 
-	        $(".contact-form input, .contact-form textarea").css('border-color',''); 
-	        $("#result").slideUp();
-	    });
-    
-   
-    
 
 });
